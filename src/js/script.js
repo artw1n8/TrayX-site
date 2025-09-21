4f6577e7f7d8309463568415d0bb4854962762b3
art tem <kalinichenkoartemiywork@gmail.com>
2025-09-21 16:22:05 +0300

    added adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 import "/src/sass/style.scss";
+
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+})
+
+const switchLangBtn = document.querySelector(".header__block-lang").querySelector("button"),
+      langContent = document.querySelector(".header__block-lang-content");
+
+      console.log(switchLangBtn)
+
+switchLangBtn.addEventListener("click", () => {
+    langContent.classList.add("show");
+});
+
+document.body.addEventListener("click", (e) => {
+    if(e.target != switchLangBtn){
+        langContent.classList.remove("show");
+    }
+});
