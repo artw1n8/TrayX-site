878b218a4aaf03ab1c4e79904eeedb7054c245c5
art tem <kalinichenkoartemiywork@gmail.com>
2025-09-29 18:14:04 +0300

    added animations and links

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ close.addEventListener("click", () => {
     menu.classList.remove("header__menu_active");
 })
 
+
 const switchLangBtn = document.querySelector(".header__block-lang").querySelector("button"),
       langContent = document.querySelector(".header__block-lang-content");
 
@@ -26,3 +27,24 @@ document.body.addEventListener("click", (e) => {
         langContent.classList.remove("show");
     }
 });
+
+const taskbarImagesPath = ["/src/img/taskbar/taskbarDark.png","/src/img/taskbar/taskbarLight1.png","/src/img/taskbar/taskbarLight2.png"];
+const taskbarImage = document.querySelector(".taskbar__img");
+let counter = 0;
+console.log(taskbarImage);
+
+const changeTaskbarImage = () => {
+    taskbarImage.style.opacity = 0;
+    setTimeout(() => {
+        taskbarImage.src = taskbarImagesPath[counter];
+        taskbarImage.style.opacity = 1;
+    }, 1500)
+
+    if(counter == taskbarImagesPath.length - 1){
+        counter = 0;
+    } else {
+        counter++;
+    }
+}
+
+setInterval(changeTaskbarImage, 6000);
